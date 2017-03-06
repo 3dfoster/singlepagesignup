c78ff526f1b97e131b2ebeb9c46e08b0894b175e
David Foster <David Foster>
2017-03-05 20:32:16 -0800

    adsf

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
+let mongoose = require('mongoose')
 let http = require('http')
 let fs = require('fs')
 
 let server = http.createServer()
 let page = fs.readFileSync('index.html')
 
+// Define the document schema for Mongoose
+let Schema = mongoose.Schema
+let emailSchema = new Schema({
+  email: String
+})
+let Email = mongoose.model('Email', emailSchema)
+
+/*
+Our server object is an EventEmitter
+When it receives an HTTP request it emits a 'request' event
+When this event occurs we perform the following code
+*/
 server.on('request', (req, res) => {
   let userAgent = req.headers['user-agent']
   let body = []
 
-  // Log error in console thereby allowing server to continue running if error is received
+  // Logging errors to console allows our server to continue running if an error is received
   req.on('error', () => {
     console.error(err)
-    // Parse chunks of data in POST request
+    // Parse chunks of data in a POST request
   }).on('data', chunk => {
     body.push(chunk)
   }).on('end', () => {
@@ -19,16 +32,78 @@ server.on('request', (req, res) => {
     body = Buffer.concat(body).toString()
 
     // Router
-    if (req.method == 'GET' && req.url == '/') {
-      // Set HTTP header information
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.write(page)
+    if (req.method == 'GET') {
+      switch (req.url) {
+        case '/':
+          // Set HTTP header information
+          res.writeHead(200, { 'Content-Type': 'text/html' })
+          res.end(page)
+          break
+
+        case '/emails':
+          res.end("<h1>This page is coming soon...</h1>")
+          break
+
+        default:
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end("<h1>404'd!</h1><p>The page you're requesting doesn't exist!</p>")
+          break
+      }
     }
-    if (req.method == 'POST' && req.url == '/') {
-      console.log('######## ISBN: ' + body + ' #########')
-      res.writeHead(200, {'Content-Type': 'text/plain'})
-      res.write("Thanks for your interest in our product! You will receive an email once it is finished")
+
+    if (req.method == 'POST') {
+      switch (req.url) {
+        case '/':
+          // Create a new mongoose model with the email our user submitted 
+          let email = new Email({ email: body })
+
+          // Connect to MongoDB
+          mongoose.connect('mongodb://localhost/test')
+          let db = mongoose.connection
+          db.on('error', console.error.bind(console, 'connection error:'))
+          db.once('open', function () {
+            // We've successfully established a conection to the database
+            console.log("Connection to Mongo database established")
+
+            // Store our user's email in the database
+            email.save()
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end("Thanks for your interest in our product! You will receive an email once it is finished")
+          })
+          break
+      }
     }
+
+              // Connect to MongoDB
+          // mongoose.connect('mongodb://localhost/test')
+          // let db = mongoose.connection
+          // db.on('error', console.error.bind(console, 'connection error:'))
+          // db.once('open', function () {
+          //   Email.find((err, emails) => {
+          //     if (err) return console.error(err)
+
+          //     res.end(emails)
+          //   })
+          // })
+
+    // if (req.method == 'POST' && req.url == '/') {
+    //   // Create a new mongoose model with the email our user submitted 
+    //   let email = new Email({ email: body })
+
+    //   // Connect to MongoDB
+    //   mongoose.connect('mongodb://localhost/test')
+    //   let db = mongoose.connection
+    //   db.on('error', console.error.bind(console, 'connection error:'))
+    //   db.once('open', function() {
+    //     // We've successfully established a conection to the database
+    //     console.log("Connection to Mongo database established")
+
+    //     // Store our user's email in the database
+    //     email.save()
+    //     res.writeHead(200, { 'Content-Type': 'text/plain' })
+    //     res.end("Thanks for your interest in our product! You will receive an email once it is finished")
+    //   })
+    // }
     
     // Error handling
     res.on('error', err => {
@@ -43,7 +118,6 @@ server.on('request', (req, res) => {
     console.log('Request Body: \t' + body)
     console.log('===================================')
 
-    res.end()
   })
 }).listen(8080)
 
